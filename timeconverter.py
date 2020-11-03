91afaffb9f58f856d0a101a9f4a289916ec15f07
60065317 <kkBBgl363>
2020-11-04 01:37:01 +0300

    added exceptions catch when user input too large number of seconds

--- timeconverter.py
+++ timeconverter.py
@@ -5,13 +5,16 @@ def convert_time(seconds: int) -> str:
     hours = seconds // 3600
     minutes = seconds // 60 - hours * 60
     seconds_tail = seconds % 60
-    calculated_time = time(hour=hours, minute=minutes, second=seconds_tail)
-    return calculated_time.isoformat()
+    try:
+        calculated_time = time(hour=hours, minute=minutes, second=seconds_tail)
+        return calculated_time.isoformat()
+    except ValueError:
+        return 'Probably value is too large!'
 
 
 if __name__ == '__main__':
     print("--------------------------------------------------\n"
-          "Hello, user! I'm time converter.\n"
+          "Hello, user! I'm Time Converter.\n"
           "I can convert time value in seconds to ISO format!\n"
           "TO EXIT just type 'exit' word and press ENTER.")
     seconds = ''
